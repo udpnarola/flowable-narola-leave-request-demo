83a7df411ca6f6d706533203bf6060c7eaaf6f8a
Uday <udp@narola.email>
2021-03-30 14:29:45 +0530

    remove unused imports

--- src/main/java/com/narola/flowablenarolaleaverequestdemo/FlowableNarolaLeaveRequestDemoApplication.java
+++ src/main/java/com/narola/flowablenarolaleaverequestdemo/FlowableNarolaLeaveRequestDemoApplication.java
@@ -1,7 +1,6 @@
 package com.narola.flowablenarolaleaverequestdemo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.narola.flowablenarolaleaverequestdemo.delegate.LeaveCancelledDelegate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/narola/flowablenarolaleaverequestdemo/delegate/LeaveCancelledDelegate.java
+++ src/main/java/com/narola/flowablenarolaleaverequestdemo/delegate/LeaveCancelledDelegate.java
@@ -1,14 +1,12 @@
 package com.narola.flowablenarolaleaverequestdemo.delegate;
 
 import com.narola.flowablenarolaleaverequestdemo.component.LeaveDelegateHelper;
-import com.narola.flowablenarolaleaverequestdemo.dto.EmailDto;
 import com.narola.flowablenarolaleaverequestdemo.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Log4j2
